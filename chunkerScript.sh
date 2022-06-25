48cc2391368a6e8d94e8b6eeb1d564656665b225
Rokib Hasan Sagar <10854164+rokibhasansagar@users.noreply.github.com>
2022-06-25 13:26:10 +0600

    Update chunkerScript.sh

--- chunkerScript.sh
+++ chunkerScript.sh
@@ -16,13 +16,13 @@ echo "::endgroup::"
 
 echo "::group:: Prepare File"
 set -xv
-printf "Downloading Media File, Please Wait...\n"
-aria2c -c -x16 -s16 "${Input_Movie_Link}" || {
-  curl -sL "${Input_Movie_Link}" -O || exit 1
-}
 function urld () { [[ "${1}" ]] || return 1; : "${1//+/ }"; echo -e "${_//%/\\x}"; }
 export unsanitized_filename=$(awk -F'/' '{printf $NF}' <<<$(urld "${Input_Movie_Link}"))
 export ConvertedName=$(sed 's/[()]//g;s/ - /\./g;s/ /\./g;s/,/\./g;s/&/and/g;s/\.\./\./g' <<<"${unsanitized_filename}")
+printf "Downloading Media File, Please Wait...\n"
+aria2c -c -x16 -s16 "${Input_Movie_Link}" -o ${ConvertedName} || {
+  curl -sL "${Input_Movie_Link}" -o ${ConvertedName} || exit 1
+}
 set +xv
 printf "\nMediaInfo of the File:\n\n"
 mediainfo "$ConvertedName"
